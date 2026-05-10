dbc6b20c8ced048dd57acb054a336937436d6490
ink1852 <hjgim07@gmail.com>
2026-05-10 18:42:30 +0900

    routing

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { createBrowserRouter } from "react-router-dom";
+import Layout from "./components/layout";
+import Profile from "./routes/profile";
+import Home from "./routes/home";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile",
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
